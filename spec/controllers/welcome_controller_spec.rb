0a67076547e7f391533fb979dc52e8c2504405be
FactorGFC <63082907+FactorGFC@users.noreply.github.com>
2022-04-11 10:09:49 -0600

    Se hace deploy

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
-      #get :index
+      #get :index #Habilitar para web de desarrollador
       get :ok
       expect(response).to have_http_status(:success)
     end
