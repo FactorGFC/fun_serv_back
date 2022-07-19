2795da23a9fa251b6645eebad11f213248cf4145
Jose Luis Delgado Carrillo <joseluisdc48@gmail.com>
2022-07-19 17:24:27 -0600

    Correcciones

--- db/migrate/20220711171003_change_column_name_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNameId < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :customer_credits_signatories, :user_id, :user_id
-    rename_column :customer_credits_signatories, :customer_credit_id, :customer_credit_id
-  end
-end
