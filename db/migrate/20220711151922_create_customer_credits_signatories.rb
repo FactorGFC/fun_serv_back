0916ad16c5edaaa975ea72433771ed09e73818d9
Jose Luis Delgado Carrillo <joseluisdc48@gmail.com>
2022-07-19 17:14:55 -0600

    Correcciones

--- db/migrate/20220711151922_create_customer_credits_signatories.rb
+++ db/migrate/20220711151922_create_customer_credits_signatories.rb
@@ -1,10 +1,10 @@
 class CreateCustomerCreditsSignatories < ActiveRecord::Migration[6.0]
   def change
     create_table :customer_credits_signatories, id: :uuid do |t|
-      # t.references :user_id, null: false, foreign_key: true
-      add_reference :customer_credits_signatories, :user,  type: :uuid, foreign_key: { to_table: :users }, null: false
-      # t.references :customer_credit_id, null: false, foreign_key: true
-      add_reference :customer_credits_signatories, :customer_credit,  type: :uuid, foreign_key: { to_table: :customer_credits }, null: false
+      t.references :user, type: :uuid, foreign_key: { to_table: :users }, null: false
+      # add_reference :customer_credits_signatories, :user,  type: :uuid, foreign_key: { to_table: :users }, null: false
+      t.references :customer_credit, type: :uuid, foreign_key: { to_table: :customer_credits }, null: false
+      # add_reference :customer_credits_signatories, :customer_credit,  type: :uuid, foreign_key: { to_table: :customer_credits }, null: false
       t.string :firmantes_token
       t.datetime :firmantes_token_expiry
       t.string :status
