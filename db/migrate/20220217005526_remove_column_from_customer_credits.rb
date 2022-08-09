dd8b903ed085c3cd20424348735cd3ccb341eb2b
Griselda Oaxaca <griselda_010@hotmail.com>
2022-08-09 09:41:58 -0600

    Se corrigen migraciones

--- db/migrate/20220217005526_remove_column_from_customer_credits.rb
+++ db/migrate/20220217005526_remove_column_from_customer_credits.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 class RemoveColumnFromCustomerCredits < ActiveRecord::Migration[6.0]
   def change
-    remove_column :customer_credits, :insurance, :decimal
-    remove_column :customer_credits, :commission, :decimal
     remove_column :customer_credits, :payment_capacity, :decimal
   end
 end
--- db/migrate/20220808215221_add_columns_commision_to_customer_credits.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsCommisionToCustomerCredits < ActiveRecord::Migration[6.0]
-  def change
-    add_column :customer_credits, :insurance, :decimal, precision: 15, scale: 4
-    add_column :customer_credits, :commission, :decimal, precision: 15, scale: 4
-  end
-end
