0ca070c306477efdc10918360646c7d60a094a00
JoseLuisDelgadoCarrillo <joseluisdc48@gmail.com>
2023-01-11 10:43:37 -0600

    Regreso cambios

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -2729,13 +2729,13 @@ class Api::V1::ReportsController < Api::V1::MasterApiController
     render json: @get_customer_pr
   end
 
-# Reporte para mostrar los datos del cliente a partir de un credito
+  # Reporte para mostrar los datos del cliente a partir de un credito
 def get_credit_customer_report
   @query = "SELECT cuc.id id_credito, cuc.rate tasa_empleado, cuc.total_requested total_solicitado, cuc.interests total_intereseses, 
   cuc.start_date fecha_credito, cuc.status status_credito, pap.value periodo_pago, 
    cus.id id_cliente,cus.name nombre_cliente,cus.customer_type tipo_cliente,cus.status status_cliente,cus.salary_period,cus.user_id id_usuario,
    cus.file_type_id id_tipo_expediente,cus.other_income otros_ingresos,cus.net_expenses egresos_netos,cus.family_expenses gastos_familiares, 
-   cus.house_rent renta,cus.credit_cp creditos_cp, cus.credit_lp creditos_lp, (cus.total_income * 4) ingreso_total, con.id id_contribuyente, 
+   cus.house_rent renta,cus.credit_cp creditos_cp, cus.credit_lp creditos_lp, cus.total_income ingreso_total, con.id id_contribuyente, 
    con.contributor_type tipo_contribuyente, con.bank banco, con.account_number cuenta_bancaria, con.clabe cuenta_clabe, 
    con.person_id id_persona_fisica, con.legal_entity_id id_persona_moral, peo.fiscal_regime pf_regimen_fiscal, 
    peo.rfc pf_rfc, peo.curp pf_curp, peo.imss pf_numero_seguro_social, peo.first_name || ' ' || peo.last_name || ' ' || peo.second_last_name pf_nombre, 
